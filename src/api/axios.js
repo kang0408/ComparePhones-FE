7f61c139d616617c0fd9a6e3bb1c63b5f7e217b3
Kang0408 <trandanhkhang482004@gmail.com>
2024-12-25 22:57:07 +0700

    proxy for API and firebase.json

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiInst = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api/v1`,
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   headers: {
     'Content-Type': 'application/json'
   }
